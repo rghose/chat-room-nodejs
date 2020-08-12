0213d707d3847997da43918e8e949811f531a1c1
Rahul Ghose <hansum.rahul@gmail.com>
2020-08-12 22:39:59 +0530

    added bootstrap and modified deploy

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ mkdir -p "$install_dir"
 git clone git@github.com:rghose/chat-room-nodejs.git $install_dir
 cd "$install_dir"
 npm install
-
+cd -
+ln -s "$install_dir" latest
+pm2 restart ecosystem.config.js --env production
